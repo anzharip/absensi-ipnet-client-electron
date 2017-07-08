8f39bedeb82828ddfc29af533b175c525fa34d1b
anzhari.p@gmail.com <anzhari.p@gmail.com>
2017-07-09 01:41:11 +0700

    Working HybridAdminKehadiran with XHR Multipart Uppload

--- HybridAdminKehadiran/js/scripts.js
+++ HybridAdminKehadiran/js/scripts.js
@@ -3,48 +3,34 @@
 // window.onload = function () { alert("It's loaded!") };
 document.getElementById("submit_button").onclick = function() {
   // Defining the variable
-  var id_karyawan = ""
-  var usr_password = ""
+  var idakun = ""
+  var pass = ""
+  var upload = ""
   // Get the value from form input
-  id_karyawan = document.getElementById('id_karyawan').value;
-  usr_password = document.getElementById('usr_password').value;
+  idakun = document.getElementById('idakun').value;
+  pass = document.getElementById('pass').value;
+  upload = document.getElementById('upload').files[0];
   //Used for logging to check whether the value succesfully pulled from the form input
-  console.log(id_karyawan);
-  console.log(usr_password);
+  console.log(idakun);
+  console.log(pass);
+  console.log(upload);
   //Asynchronous HTTP request to the API server
   var xmlHttp = new XMLHttpRequest();
-  var url = "http://localhost:18080/kehadiran"
-  var query = "?" + "idakun=" + id_karyawan + "&" +
-              "tgl_lahir=" + tgl_lahir + "&" +
-              "tahun=" + tahun + "&" +
-              "bulan=" + bulan + "&" +
-              "usr_pass=" + usr_password;
+  var url = "http://localhost:18080/akun/create"
+  var formData = new FormData();
+  formData.append("upload", upload);
+  formData.append("idakun", idakun);
+  formData.append("pass", pass);
   xmlHttp.onreadystatechange = function() {
       if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
           callback(xmlHttp.responseText);
   }
-  xmlHttp.open("GET", url + query, true); // true for asynchronous
-  xmlHttp.send(null);
+  xmlHttp.open("POST", url, true); // true for asynchronous
+  xmlHttp.send(formData);
   //Callback function when the asynchronous request succeeed
   var callback = function(responseText) {
     var responseText = JSON.parse(responseText);
     console.log(responseText);
-    //Render the response text into the HTML table
-    var tablebody = document.getElementById("tablebody");
-    responseText.data.forEach(function(array){
-      console.log(array[1]);
-      var row = tablebody.insertRow();
-      var tanggal = row.insertCell();
-      tanggal.innerHTML = array[1];
-      var nama = row.insertCell();
-      nama.innerHTML = array[3];
-      var divisi = row.insertCell();
-      divisi.innerHTML = array[5];
-      var masuk = row.insertCell();
-      masuk.innerHTML = array[6];
-      var pulang = row.insertCell();
-      pulang.innerHTML = array[7];
-    });
     //Show the status and message through alert
     var alertMessage = "Status: " + responseText.status + "\n" +
                     "Pesan: " + responseText.message;
